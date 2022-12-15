06d70e23b81c812c4dd7c5afafbcc9d15f5e2f8f
erikluo <erikluo@tencent.com>
2022-12-16 00:54:09 +0800

    a

--- run_ss.sh
+++ run_ss.sh
@@ -1,4 +1,4 @@
 echo $SS_HOST
 echo $SS_PASSWD
 ln -sf ./v2ray-plugin_linux_amd64 v2ray
-./shadowsocks2-linux  -s 'ss://AEAD_CHACHA20_POLY1305:$SS_PASSWD@:28488' -verbose     -plugin v2ray -plugin-opts "server"
+./shadowsocks2-linux  -s "ss://AEAD_CHACHA20_POLY1305:$SS_PASSWD@:28488" -verbose     -plugin v2ray -plugin-opts "server"
